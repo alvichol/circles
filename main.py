4672668e2e164193d9550e8796e43f53843b6680
Alisa <alvichol@gmail.com>
2020-11-10 19:54:16 +0300

    circles updated

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.run)
+        self.do_paint = False
+
+    def run(self):
+        self.do_paint = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        r = randint(0, self.height())
+        x, y = randint(0, self.height()), randint(0, self.height())
+        qp.drawEllipse(x - r, y - r, x + r, y + r)
+        self.do_paint = False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
